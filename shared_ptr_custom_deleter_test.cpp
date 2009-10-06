13d8d8225fc79b5aa3246e1a05ef73e6466c6f73
Karol Barski <karol@barski.eu>
2009-10-06 22:59:34 +0200

    Updated README

--- shared_ptr_custom_deleter_test.cpp
+++ shared_ptr_custom_deleter_test.cpp
@@ -1,11 +1,11 @@
+/* -*- mode: c++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+   vim: set ts=4 sts=4 sw=4 et:
+*/
 /**
  *  @file   shared_ptr_custom_deleter_test.cpp
  *  @brief  Test program for custom deleter taking argument as pointer to pointer
  *  @author Karol Barski
  *  @date   Fri Sep 11 12:56:20 2009
- *
- *  @par history
- *  @li Fri Sep 11 12:56:20 2009 Karol Barski: Created
  *
  */
 
@@ -19,82 +19,69 @@
 
 struct TestStruct
 {
-	explicit TestStruct(int counter)
-		: counter_(counter)
-	{
-		std::cout << "TestStruct create " << counter_ << "\n";
-	};
+    explicit TestStruct(int counter)
+        : counter_(counter)
+    {
+        std::cout << "TestStruct create " << counter_ << "\n";
+    };
 
-	~TestStruct()
-	{
-		std::cout << "TestStruct destroy " << counter_ << "\n";
-	}
+    ~TestStruct()
+    {
+        std::cout << "TestStruct destroy " << counter_ << "\n";
+    }
 private:
-	int counter_;
+    int counter_;
 };
 
 
 TestStruct *createTest(int counter)
 {
-	return new TestStruct(counter);
+    return new TestStruct(counter);
 }
 
 void deleteTest1(TestStruct *test)
 {
-	if(!test)
-	{
-		std::cout << __func__ << "Null contained pointer passed\n";
-	}
-
-	delete test;
+    if(!test) {
+        std::cout << __func__ << "Null contained pointer passed\n";
+    }
+    delete test;
 }
 
 void deleteTest2(TestStruct **test)
 {
-	if(!test)
-	{
-		std::cout << __func__ << "Null pointer passed\n";
-	}
+    if(!test) {
+        std::cout << __func__ << "Null pointer passed\n";
+    }
 
-	if(!*test)
-	{
-		std::cout << __func__ << "Null contained pointer passed\n";
-	}
+    if(!*test) {
+        std::cout << __func__ << "Null contained pointer passed\n";
+    }
 
-	delete *test;
-	*test = 0;
+    delete *test;
+    *test = 0;
 }
 
 TestStruct **derefPtr(TestStruct *&test)
 {
-	if(!test)
-	{
-		return 0;
-	}
-	return &test;
+    if(!test) {
+        return 0;
+    }
+    return &test;
 }
 
 int main(int argc, char **argv)
 {
-	int testCount = 0;
-	boost::shared_ptr<TestStruct> testPtr(createTest(++testCount), &deleteTest1);
+    int testCount = 0;
+    boost::shared_ptr<TestStruct> testPtr(createTest(++testCount), &deleteTest1);
+    boost::shared_ptr<TestStruct> testPtr1(createTest(++testCount),
+                                           boost::bind(&deleteTest1, _1));
 
-	boost::shared_ptr<TestStruct> testPtr1(createTest(++testCount),
-					      boost::bind(&deleteTest1,
-							  _1
-						      ));
+    boost::shared_ptr<TestStruct> testPtr2(createTest(++testCount),
+                                           boost::bind(&deleteTest2,
+                                                       boost::bind(&derefPtr, _1)));
 
-	boost::shared_ptr<TestStruct> testPtr2(createTest(++testCount),
-					      boost::bind(&deleteTest2,
-							  boost::bind(&derefPtr, _1)
-						      ));
-
-	boost::shared_ptr<TestStruct> testPtr3(createTest(++testCount),
-					      boost::bind(&deleteTest2,
-							  boost::bind(&boost::addressof<TestStruct*>,
-								      _1)
-						      ));
-
-	std::cout << "working\n";
-
+    boost::shared_ptr<TestStruct> testPtr3(createTest(++testCount),
+                                           boost::bind(&deleteTest2,
+                                                       boost::bind(&boost::addressof<TestStruct*>, _1)));
+    std::cout << "working\n";
 }
